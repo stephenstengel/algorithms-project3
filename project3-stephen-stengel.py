64c49a1eb4d3112e856133efab1ef4e9cd291e34
Stephen <stephen.stengel@cwu.edu>
2021-02-24 05:30:22 -0800

    Discovered that I cannot time the mapping and reduction separately.

--- project3-stephen-stengel.py
+++ project3-stephen-stengel.py
@@ -6,44 +6,109 @@
 #  Stephen Stengel  <stephen.stengel@cwu.edu>  40819903
 #  Project3
 
+#Note
+#I discovered that spark executes transformations, mapping, and
+#reductions, using Lazy execution. It doesn't compute anything until an
+#answer is needed, then all the mapping is done at once. So I can't time
+#the individual portions of the program-- only the total runtime of each
+#word count.
+#See: https://stackoverflow.com/questions/36269179/how-to-time-a-transformation-in-spark-given-lazy-execution-style
+
 from pyspark import SparkContext
 import re #python regular expressions.
 from operator import add
 from operator import itemgetter #for sorting by an unnamed key
+import time #for timing the reductions.
+import os #removing created files
 
 
 def main(args):
-	print("Hi lol")
-	
-	myFile = "file:///home/stephen/Documents/School/WarIsARacket_djvu.txt"  
 	sc = SparkContext()
 	
+	file1 = "WarIsARacket_djvu.txt"
+	file2 = "pride-and-prejudice.txt"
+	
+	print("Generating temporary text files...")
+	file3 = fileMultiplier(file2, 10)
+	file4 = fileMultiplier(file2, 100)
+	file5 = fileMultiplier(file2, 1000)
+	os.system("sync")
+	print("Done generating texts!")
+	
+	#put filenames into array
+	# ~ fNameArray = [file1, file2, file3, file4, file5]
+	# ~ fNameArray = [file1, file2, file3, file4]
+	fNameArray = [file1, file2, file3]
+	
+	isPrintLists = True
+	isPrintTimes = True
+	
+	outputTimes = []
+	
+	print("Starting timing tests...")
+	for i in range(0, len(fNameArray) ):
+		outputTimes.append( runTest( fNameArray[i], sc, isPrintLists, isPrintTimes ) )
+	
+	print("Time tests done!")
+	
+	os.system("rm " + str(file3) + " " + str(file4) + " " + str(file5) )
+	
+	return 0
+
+
+#Runs a test of map and reduce on the input file.
+#Prints the final word list if isPrintList is True. Prints times if isPrintTimes is True.
+#Returns the time for mapping and time for reduction.
+def runTest(fileName, sc, isPrintList, isPrintTime):
+	outsideStart = time.time()###
+	outlist, timeLoad, timeMap, timeReduction = capWordCounter(fileName, sc)
+	outsideEnd = time.time()###
+	
+	print("Total time for " + fileName + ":\t" + str( outsideEnd - outsideStart ))###
+	
+	if isPrintList or isPrintTime:
+		if isPrintTime:
+			print(fileName + "\tload: " + str(timeLoad) + "\tmap: " + str(timeMap) + "\treduce: " + str(timeReduction))
+		if isPrintList:
+			print("List for " + fileName + "...")
+			printOutput(outlist)
+
+	return timeLoad, timeMap, timeReduction
+
+
+#Makes a count of the number of each capitalized word in an input text.
+#Returns a list, map time, reduction time. Params: Input file, spark context.
+def capWordCounter(myFile, sc):
+	print("Loading " + str(myFile) + "...")
+	loadStart = time.time()
 	theText = sc.textFile(myFile)
+	loadEnd = time.time()
+	print("Done loading " + str(myFile) + "!")
+	
+	#start map timer
+	startMap = time.time()
 	allWords = theText.flatMap(lambda line: line.split(" "))
 	
-	#capMapper() removes words that are not capitalized.
+	#remove words that are not capitalized.
 	capWords = allWords.flatMap(capMapper)
 
 	#Assign each word the value of 1.
 	wordMap = capWords.map(lambda word: (word, 1))
+	#end map timer
+	endMap = time.time()
 	
-	#Reduce the word list by adding the separate occurrences. The count of each word remains.
-	wordCounts = wordMap.reduceByKey(add).collect()  #collect turns it into a list.
-	# ~ wordCounts = wordMap.reduceByKey(add)
+	#Reduce the word list by summing the separate occurrences.
+	startReduce = time.time()
+	wordCounts = wordMap.reduceByKey(add)
+	endReduce = time.time()
 	
-	# ~ print(sorted(wordCounts))
+	colStart = time.time()############
+	wordCountsList = wordCounts.collect()  #collect turns it into a list.
+	colEnd = time.time()#############
+	print("Time to collect list:\t" + str( colEnd - colStart ))#############
 	
-	# save the counts to output
-	# ~ wordCounts.saveAsTextFile("out")
-	
-	for cWord in sorted(wordCounts, key=itemgetter(1), reverse=True):
-		firstItemGet = itemgetter(0)
-		secondItemGet = itemgetter(1)
-		if secondItemGet(cWord) > 5:
-			print( str(firstItemGet(cWord)) + "\t" + str(secondItemGet(cWord)) )
+	return wordCountsList, (loadEnd - loadStart), (endMap - startMap), (endReduce - startReduce)
 
-	
-	return 0
 
 #For use in flatMap(). Returns the word only if Capitalized. Else nothing.
 def capMapper(myWord):
@@ -59,6 +124,31 @@ def isCapWord(myWord):
 		return True
 	else:
 		return False
+
+#Prints the output of reduction. Max ten lines.
+def printOutput(wordCountsList):
+	count = 1
+	for cWord in sorted(wordCountsList, key=itemgetter(1), reverse=True):
+		if count > 10:
+			break
+		firstItemGet = itemgetter(0)
+		secondItemGet = itemgetter(1)
+		print( str(firstItemGet(cWord)) + "\t" + str(secondItemGet(cWord)) )
+		count += 1
+
+#Creates a file a multiple larger than input file by rewiting lines.
+#Returns the name of the file.
+def fileMultiplier(inFileName, mult):
+	if mult < 2:
+		mult = 2
+	outFileName = inFileName + "-" + str(mult) + ".txt"
+	with open(inFileName, 'r') as infile, open(outFileName, 'w') as outfile:
+		for line in infile:
+			for i in range(0, mult):
+				outfile.write(line)
+
+	return outFileName
+
 
 if __name__ == '__main__':
 	import sys
